e62191438bc0e69ed0ae763d0c52da913e7902c2
Yinson Tso <yinson.tso@gmail.com>
2023-05-01 14:42:08 -0400

    list crud

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,43 @@
 def create_an_empty_list():
+    l = []
+    return(l)
     return None
 
 def create_a_list():
+    l = []
+    for n in range(4):
+        l.append(n)
+    return l
     return None
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
+    l.insert(0, element)
+    return l
     return None
 
 def remove_element_from_end_of_list(l):
+    l.remove(l[-1])
+    return l
     return None
 
 def remove_element_from_start_of_list(l):
+    l.remove(l[0])
+    return l
     return None
 
 def retrieve_first_element_from_list(l):
+    first = l[0]
+    return first
     return None
 
 def retrieve_element_from_index(l, index):
+    return(l[index])
     return None
 
 def retrieve_last_element_from_list(l):
+    return(l[-1])
     return None
